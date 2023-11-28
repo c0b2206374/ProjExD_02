29ec2879bf2abf6180dccf243eb7a923e9475845
c0b2206374 <c0b2206374@edu.teu.ac.jp>
2023-11-28 14:55:51 +0900

    練習4_こうかとんと爆弾を画面の外に出さない

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,13 +1,27 @@
+import random
 import sys
 import pygame as pg
-import random 
+
 
 WIDTH, HEIGHT = 1600, 900
 
-delta = {pg.K_UP:(0,-5),
-         pg.K_DOWN:(0,5),
-         pg.K_LEFT:(-5,0),
-         pg.K_RIGHT:(5,0)}
+delta = { 
+    pg.K_UP: (0, -5), 
+    pg.K_DOWN: (0, 5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (5, 0)
+}
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  
+        tate = False
+    return yoko, tate
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -16,40 +30,43 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect() 
-    kk_rct.center = 900, 400
+    kk_rct.center = 900, 400 
+    bb_img = pg.Surface((20, 20))   
+    bb_img.set_colorkey((0, 0, 0)) 
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) 
+    bb_rct = bb_img.get_rect()  
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, 5  
+
     clock = pg.time.Clock()
     tmr = 0
-    bom = pg.Surface((20, 20))
-    pg.draw.circle(bom, (255, 0, 0), (10, 10), 10)
-    bom.set_colorkey((0, 0, 0))
-    bom_rct = bom.get_rect()
-    bom_rct.centerx = random.randint(0, WIDTH)
-    bom_rct.centery = random.randint(0, HEIGHT)
-    vx,vy = +5,+5
-
-
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+            
         key_lst = pg.key.get_pressed()
-        sum_mv = [0,0]
-        for k,tpl in delta.items():
-            if key_lst[k]:
-                sum_mv[0]+=tpl[0]
-                sum_mv[1]+=tpl[1]
-                
+        sum_mv = [0, 0]
+        for k, tpl in delta.items():
+            if key_lst[k]:  
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
+        
 
         screen.blit(bg_img, [0, 0])
-
-        bom_rct.move_ip(vx,vy)
-        screen.blit(bom,bom_rct)
-        
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
-        screen.blit(kk_img, kk_rct)
-
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kk_img, kk_rct)  
+        bb_rct.move_ip(vx, vy) 
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  
+            vx *= -1
+        if not tate:  
+            vy *= -1
+        bb_rct.move_ip(vx, vy) 
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
